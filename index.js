2ba3702e0bfb15a74d7e2b8610c01259fc584591
Muhamad Anjar <anjar@btech.id>
2026-05-06 21:46:34 +0700

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+import 'dotenv/config'
+import express from 'express'
+import multer from 'multer'
+import { GoogleGenAI } from "@google/genai"
+
+console.log(process.env.GEMINI_API_KEY)
+
+const app = express()
+const upload = multer()
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
+
+const GEMINI_MODEL = "gemini-2.5-flash";
+
+app.use(express.json());
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server Ready on http://localhost:${port}`));
+
+app.post('/generate-text', async (req, res)=>{
+    const { prompt } = req.body;
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: prompt
+        })
+
+        res.status(200).json({ result: response.text })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+
+app.post('/generate-from-image', upload.single('image'), async (req, res) => {
+    const { prompt } = req.body
+    const base64Image = req.file.buffer.toString('base64');
+
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt, type: 'text'},
+                { inlineData: { data: base64Image, mimeType: req.file.mimetype }}
+            ]
+        })
+        res.status(200).json({ result: response.text})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message})
+    }
+})
+
+app.post('/generate-from-document', upload.single('document'), async (req, res) => {
+    const { prompt } = req.body;
+
+    const base64Image = req.file.buffer.toString('base64');
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt ?? "Tolong buatkan ringkasan dari dokumen ini", type: 'text'},
+                { inlineData: { data: base64Image, mimeType: req.file.mimetype }}
+            ]
+        })
+        res.status(200).json({ result: response.text })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message })
+    }
+})
+
+
+app.post('/generate-from-audio', upload.single('audio'), async (req, res)=>{
+    const { prompt } = req.body;
+
+    const base64Image = req.file.buffer.toString('base64');
+    try {
+        const response = await ai.models.generateContent({
+            model: GEMINI_MODEL,
+            contents: [
+                { text: prompt ?? "Tolong buatkan transkip dari rekaman berikut", type: 'text'},
+                { inlineData: { data: base64Image, mimeType: req.file.mimetype }}
+            ]
+        })
+        res.status(200).json({ result: response.text })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message })
+    }
+})
